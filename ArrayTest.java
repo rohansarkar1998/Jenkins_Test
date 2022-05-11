c55d40089df62426713728e0829996a08a669c0c
rohan1998 <rohansarkarmsit@gmail.com>
2022-05-11 12:39:27 +0530

    remote push

---
+++ ArrayTest.java
@@ -0,0 +1,16 @@
+package HackerrankPrac;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4};
+
+        for(int i = 0;i<array.length;i++)
+        {
+            if(i == 2)
+            {
+
+            }
+        }
+
+    }
+}
